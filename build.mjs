1b015c8294ebb8862964563c7bd2eb11cd84f670
Refringe <brownelltyler@gmail.com>
2023-07-01 19:12:46 -0400

    Continued build script refactor

--- build.mjs
+++ build.mjs
@@ -4,6 +4,7 @@ import fs from "fs";
 import os from "os";
 import path from "path";
 import archiver from "archiver";
+import winston from "winston";
 
 const ignore = [
     ".git",
@@ -27,95 +28,170 @@ const allowedSubdirectories = {
     "node_modules": ["json5"]
 };
 
-async function loadPackageJson(currentDir)
-{
+const logLevels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    success: 3
+};
+
+const logColors = {
+    error: "red",
+    warn: "yellow",
+    info: "grey",
+    success: "green"
+};
+
+winston.addColors(logColors);
+
+const logger = winston.createLogger({
+    levels: logLevels,
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.printf(info => {
+            return `${info.level}: ${info.message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console({
+            level: "success" // Set the log level to 'success'
+        })
+    ]
+});
+
+async function main() {
+    const currentDir = getCurrentDirectory();
+
+    const packageJson = await loadPackageJson(currentDir);
+    const projectName = createProjectName(packageJson);
+    logger.log("success", `Project name created: ${projectName}`);
+
+    const distDir = await cleanAndCreateDistDirectory(currentDir);
+    logger.log("success", `Distribution directory cleaned: ${distDir}`);
+
+    const projectDir = await createTemporaryDirectoryWithProjectName(projectName);
+    logger.log("success", `Created temporary working directory: ${projectDir}`);
+
+    logger.log("info", "Beginning copy operation...");
+    await copyFiles(currentDir, projectDir);
+    logger.log("success", "Files have been successfully copied to temporary directory.");
+
+    logger.log("info", "Beginning folder compression...");
+    const zipFilePath = path.join(path.dirname(projectDir), `${projectName}.zip`);
+    await createZipFile(projectDir, zipFilePath);
+    logger.log("success", `Created package: ${zipFilePath}`);
+
+    const zipFileInProjectDir = path.join(projectDir, `${projectName}.zip`);
+    await fs.promises.rename(zipFilePath, zipFileInProjectDir);
+    logger.log("success", `Moved package: ${zipFileInProjectDir}`);
+
+    await fs.promises.rename(projectDir, path.join(distDir));
+    logger.log("success", "Relocated temporary directory into project distribution directory.");
+
+    await fs.promises.rm(projectDir, { force: true, recursive: true });
+    logger.log("success", "Cleaned temporary directory.");
+
+    logger.log("success", "Build script completed successfully!");
+    logger.log("success", "Your mod package has been created in the 'dist' directory:");
+    logger.log("success", path.join(distDir, `${projectName}.zip`));
+    logger.log("warn", "You look great today!");
+}
+
+function getCurrentDirectory() {
+    return path.dirname(new URL(import.meta.url).pathname);
+}
+
+async function loadPackageJson(currentDir) {
     const packageJsonPath = path.join(currentDir, "package.json");
     const packageJsonContent = await fs.promises.readFile(packageJsonPath, "utf-8");
     return JSON.parse(packageJsonContent);
 }
 
-function createProjectName(packageJson)
-{
+function createProjectName(packageJson) {
     const author = packageJson.author.replace(/\W/g, "").toLowerCase();
     const name = packageJson.name.replace(/\W/g, "").toLowerCase();
     const version = packageJson.version;
     return `${author}-${name}-${version}`;
 }
 
-async function createTemporaryDirectoryWithProjectName(projectName)
-{
-    const tempDir = await fs.promises.mkdtemp(path.join(os.tmpdir(), "my-build-"));
+async function cleanAndCreateDistDirectory(projectDir) {
+    const distPath = path.join(projectDir, "dist");
+    await fs.promises.rm(distPath, { force: true, recursive: true });
+
+    await fs.promises.mkdir(distPath);
+    return distPath;
+}
+
+async function createTemporaryDirectoryWithProjectName(projectName) {
+    const tempDir = await fs.promises.mkdtemp(path.join(os.tmpdir(), "spt-mod-build-"));
     const projectDir = path.join(tempDir, projectName);
     await fs.promises.mkdir(projectDir);
     return projectDir;
 }
 
-async function copyFiles(srcDir, destDir)
-{
-    try
-    {
+async function copyFiles(srcDir, destDir) {
+    try {
         const entries = await fs.promises.readdir(srcDir, { withFileTypes: true });
 
-        for (const entry of entries)
-        {
+        for (const entry of entries) {
             const srcPath = path.join(srcDir, entry.name);
             const destPath = path.join(destDir, entry.name);
 
-            if (ignore.includes(entry.name))
-            {
-                console.log(`Ignoring: ${entry.name}`);
+            if (shouldIgnore(entry, srcDir)) {
+                logger.log("info", `Ignored: ${srcPath}`);
                 continue;
             }
 
-            if (entry.isDirectory())
-            {
-                const parentDirName = path.basename(srcDir);
-
-                if (parentDirName === "node_modules" && !allowedSubdirectories[parentDirName]?.includes(entry.name))
-                {
-                    console.log(`Ignoring: ${entry.name}`);
-                    continue;
-                }
-
+            if (entry.isDirectory()) {
                 await fs.promises.mkdir(destPath);
                 await copyFiles(srcPath, destPath);
-            }
-            else
-            {
-                // Ignore files that are directly within the allowedSubdirectories
-                if (Object.keys(allowedSubdirectories).includes(path.basename(srcDir))) {
-                    console.log(`Ignoring: ${entry.name}`);
-                    continue;
-                }
-
+            } else {
                 await fs.promises.copyFile(srcPath, destPath);
-                console.log(`Copied: ${srcPath} to ${destPath}`);
+                logger.log("info", `Copied: ${srcPath}`);
             }
         }
-    }
-    catch (err)
-    {
-        console.error("Error copying files:", err);
+    } catch (err) {
+        logger.log("error", "Error copying files: " + err);
     }
 }
 
-async function createZipFile(directoryToZip, zipFilePath)
-{
-    return new Promise((resolve, reject) =>
-    {
+function shouldIgnore(entry, srcDir) {
+    if (ignore.includes(entry.name)) {
+        return true;
+    }
+
+    const parentDirName = path.basename(srcDir);
+    if (Object.keys(allowedSubdirectories).includes(parentDirName)) {
+        if (allowedSubdirectories[parentDirName].includes(entry.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    return false;
+}
+
+async function createZipFile(directoryToZip, zipFilePath) {
+    return new Promise((resolve, reject) => {
         const output = fs.createWriteStream(zipFilePath);
         const archive = archiver("zip", {
             zlib: { level: 9 } // Sets the compression level.
         });
 
-        output.on("close", function()
-        {
-            console.log("Archiver has been finalized and the output file descriptor has closed.");
+        output.on("close", function () {
+            logger.log("info", "Archiver has finalized. The output and the file descriptor have closed.");
             resolve();
         });
 
-        archive.on("error", function(err)
-        {
+        archive.on("warning", function(err) {
+            if (err.code === "ENOENT") {
+                logger.log("warn", `Archiver issued a warning: ${err.code} - ${err.message}`);
+            } else {
+                reject(err);
+            }
+        });
+
+        archive.on("error", function (err) {
             reject(err);
         });
 
@@ -125,41 +201,7 @@ async function createZipFile(directoryToZip, zipFilePath)
     });
 }
 
-async function cleanAndCreateDistDirectory(projectDir)
-{
-    const distPath = path.join(projectDir, "dist");
-    await fs.promises.rm(distPath, { force: true, recursive: true });
-
-    await fs.promises.mkdir(distPath);
-    return distPath;
-}
-
 // Entry point
-(async () =>
-{
-    const currentDir = path.dirname(new URL(import.meta.url).pathname);
-    const packageJson = await loadPackageJson(currentDir);
-    const projectName = createProjectName(packageJson);
-    const projectDir = await createTemporaryDirectoryWithProjectName(projectName);
-
-    console.log(`Temporary directory with project name created at: ${projectDir}`);
-
-    await copyFiles(currentDir, projectDir);
-    console.log("Files copied successfully!");
-
-    const zipFilePath = path.join(os.tmpdir(), `${projectName}.zip`);
-    await createZipFile(projectDir, zipFilePath);
-    console.log(`Zip file created at: ${zipFilePath}`);
-
-    const zipFileInProjectDir = path.join(projectDir, `${projectName}.zip`);
-    await fs.promises.rename(zipFilePath, zipFileInProjectDir);
-    console.log(`Zip file moved to: ${zipFileInProjectDir}`);
-
-    const distDir = await cleanAndCreateDistDirectory(currentDir);
-    await copyFiles(projectDir, path.join(distDir));    
-
-    // Clean up temporary directory
-    await fs.promises.rm(projectDir, { force: true, recursive: true });
-
-    console.log("Script completed successfully.");
-})();
+main().catch(err => {
+    logger.log("error", "An error occurred: " + err);
+});
